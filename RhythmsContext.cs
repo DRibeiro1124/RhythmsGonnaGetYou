abf56b6dd919a52738f05135d27b02d2958e5eb6
Dino Ribeiro <DRibeiro1124@gmail.com>
2023-02-14 20:51:27 -0500

    sample code to count number of bands

--- RhythmsContext.cs
+++ RhythmsContext.cs
@@ -4,6 +4,12 @@ namespace RhythmsGonnaGetYou
 {
     public class RhythmsContext : DbContext
     {
+        //           C# Class
+        //             |     DB Table Name
+        //             |     |   
+        //             V     V
+        public DbSet<Band> Bands { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseNpgsql("server=localhost;database=Music");
