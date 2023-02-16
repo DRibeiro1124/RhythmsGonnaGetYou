bd0a0c1aaa026546e0836b0d7e853053cbdb4c09
Dino Ribeiro <DRibeiro1124@gmail.com>
2023-02-15 23:23:08 -0500

    app is working but a few bugs to deal with

--- Program.cs
+++ Program.cs
@@ -14,6 +14,166 @@ namespace RhythmsGonnaGetYou
 
             bool keepGoing = true;
 
+            while (keepGoing)
+            {
+                var input = PromptInput("Please select from the following menu options: \r\n(C)reate, (V)iew, (U)pdate a band or (Q)uit program ").ToUpper();
+
+                switch (input)
+                {
+                    case "C":
+                        input = PromptInput("Create a new (B)and, (A)lbum, or a (S)ong ").ToUpper();
+
+                        switch (input)
+                        {
+                            case "B":
+                                context.Bands.Add(CreateBand());
+                                context.SaveChanges();
+                                break;
+
+                            case "A":
+                                Album newAlbum = CreateAlbum();
+                                input = PromptInput("What is the name of the band for this new album? ");
+                                var otherBandByName = context.Bands.FirstOrDefault(band => band.Name.ToUpper().Contains(input.ToUpper()));
+
+                                if (otherBandByName != null)
+                                {
+                                    Album existingBandAlbum = CreateAlbum();
+                                    existingBandAlbum.BandId = otherBandByName.Id;
+                                    context.Add(existingBandAlbum);
+                                    context.SaveChanges();
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"This band does not exist in our database...");
+                                }
+                                break;
+
+                            case "S":
+                                input = PromptInput("What is the name of the album that contains new song? ");
+                                Album AlbumByName = context.Albums.FirstOrDefault(album => album.Title.ToUpper().Contains(input.ToUpper()));
+
+                                if (AlbumByName != null)
+                                {
+                                    Song newSong = CreateSong();
+                                    newSong.AlbumId = AlbumByName.Id;
+                                    context.Add(newSong);
+                                    context.SaveChanges();
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"That albums does not exist in our database...");
+                                }
+                                break;
+                        }
+
+                        break;
+
+                    case "V":
+
+                        input = PromptInput("Please select from the following numeric menu: \r\n1- View all bands \r\n2- View all albums from a band \r\n3- View all signed bands \r\n4- View all bands unsigned \r\n5- View all albums in order by release date");
+
+                        List<Band> bandsList = context.Bands.Include(band => band.Albums).ToList();
+
+                        switch (input)
+                        {
+                            case "1":
+
+                                Console.WriteLine(String.Format(("{0,20} | {1,-20} | {2,-8} | {3, -20}"), "Band Name", "Country of Origin", "Numbers", "Website"));
+                                Console.WriteLine($"___________________________________________________________________");
+
+                                foreach (Band bands in bandsList)
+                                {
+                                    Console.WriteLine(String.Format(("{0,-20} | {1,-20} | {2,-8} | {3,-20}"), bands.Name, bands.CountryOfOrigin, bands.NumberOfMembers, bands.Website));
+                                }
+                                break;
+
+                            case "2":
+
+                                input = PromptInput("Which band would you like to see albums from? ");
+
+                                var newBandByName = context.Bands.FirstOrDefault(band => band.Name.ToUpper().Contains(input.ToUpper()));
+
+                                if (newBandByName != null)
+                                {
+                                    Console.WriteLine($"\r\nAlbums by {newBandByName.Name} ");
+                                    Console.WriteLine(String.Format(("{0,-20} | {1,-20} | {2,-20}"), "Album Title", "Explicit?", "Release Date"));
+                                    Console.WriteLine($"________________________________________________________________");
+
+                                    foreach (Album album in newBandByName.Albums)
+                                    {
+                                        Console.WriteLine(String.Format(("{0,-20} | {1,-20} | {2,-20}"), album.Title, album.IsExplicit, album.ReleaseDate));
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Band could not be found...");
+
+                                }
+                                break;
+
+                            case "3":
+                                bandsList = bandsList.Where(band => band.IsSigned).ToList();
+                                Console.WriteLine($"These are the bands signed: ");
+
+                                foreach (Band band in bandsList)
+                                {
+                                    Console.WriteLine(band.Name);
+                                }
+                                Console.WriteLine();
+                                break;
+
+                            case "4":
+                                bandsList = bandsList.Where(band => !band.IsSigned).ToList();
+                                Console.WriteLine($"These bands are left unsigned: ");
+
+                                foreach (Band band in bandsList)
+                                {
+                                    Console.WriteLine(band.Name);
+                                }
+                                Console.WriteLine();
+                                break;
+
+                            case "5":
+                                List<Album> albumsList = context.Albums.OrderBy(album => album.ReleaseDate).ToList();
+
+                                foreach (Album album in albumsList)
+                                {
+                                    Console.WriteLine($"{album.Title} was released on {album.ReleaseDate}");
+                                }
+                                Console.WriteLine();
+                                break;
+                        }
+                        break;
+
+                    case "U":
+
+                        input = PromptInput("What is the name of the band you want to sign or release? ");
+                        var bandByName = context.Bands.FirstOrDefault(band => band.Name.ToUpper().Contains(input.ToUpper()));
+
+                        if (bandByName != null)
+                        {
+                            var isSignedOrNotSigned = (bandByName.IsSigned ? "signed" : "not signed");
+                            Console.WriteLine($"{bandByName} is currently {isSignedOrNotSigned} ");
+                            input = PromptInput($"Would you like to (S)ign or (R)elease {bandByName.Name}");
+                            bandByName.IsSigned = (input.ToUpper() == "S" ? true : false);
+                            context.Update(bandByName);
+                            context.SaveChanges();
+                            isSignedOrNotSigned = (bandByName.IsSigned ? "signed" : "not signed");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Sorry, band not found...");
+                        }
+
+                        break;
+
+                    default:
+                        keepGoing = false;
+                        break;
+                }
+            }
+
+
         }
 
         static void DisplayGreeting()
